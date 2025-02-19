7a6d45f38d5e91ce673491cbd52312ae53931aa4
atjavier <atjavier1@up.edu.ph>
2025-02-19 12:28:18 +0800

    docs: added README.md file and documentations in index.js

---
+++ README.md
@@ -0,0 +1,21 @@
+# Name: Adrian Jericho T. Javier
+# Section: CMSC 100 U-4L:
+# CMSC 100 Exercise 3
+The program provides a set of functions to validate, reverse, and store passwords in an object.
+
+## Features
+1. **Password Validation**
+    - Validates if two passwords match
+    - Ensures the password meets the following criteria:
+        - At least 8 characters long.
+        - Contains at least 1 number, 1 uppercase character, and 1 lowercase character.
+2. **Password Reversal**
+    - Reverses a given password string
+3. **Password Storage**
+    - Store user credentials (name and password) in an object
+    - If the provided passwords are valid, the reversed password is stored as the 'newpassword'
+    - If the passwords are invalid, the first provided password is stored as the 'newpassword'
+
+## How to Use
+1. Clone the respository or download the script.
+2. Use the functions as needed to validaet, reverse, and store passwords.
--- index.js
+++ index.js
@@ -3,13 +3,13 @@ function validatePassword(input, password) {
     let length = input.length;
 
     if (input === password) {
-        if (input.length >= 8) {
+        if (input.length >= 8) { // checks if input is at least 8 characters
             for (let i = 0; i != length; i++) {
-                if (input[i] >= "A" && input[i] <= "Z") {
+                if (input[i] >= "A" && input[i] <= "Z") { //checks for one upper case letter by comparing using ascii values
                     for (let j = 0; j != length; j++) {
-                        if (input[j] >= "a" && input[j] <= "z") {
+                        if (input[j] >= "a" && input[j] <= "z") { //checks for one lower case letter by comparing using ascii values
                             for (let k = 0; k != length; k++) {
-                                if (input[k] >= "0" && input[k] <= "9") {
+                                if (input[k] >= "0" && input[k] <= "9") { //checks for one number character by comparing using ascii values
                                     return true;
                                 }
                             }
@@ -27,7 +27,7 @@ function validatePassword(input, password) {
 function stringReverse(password) {
     let length = password.length - 1;
     let revPw = "";
-    for (let i = length; i != -1; i--) {
+    for (let i = length; i != -1; i--) {    //traverses the parameter of stringReverse from the last index and concats each letter to a new string
         revPw = revPw + password[i];
     }
     return revPw;
@@ -35,12 +35,12 @@ function stringReverse(password) {
 
 function storePassword(name, pass1, pass2) {
     let newpass;
-    if (validatePassword(pass1, pass2)) {
-        newpass = stringReverse(pass1);
-    } else {
+    if (validatePassword(pass1, pass2)) {   //checks if entered passwords are valid
+        newpass = stringReverse(pass1); //reverse if valid
+    } else {    // if not valid, first entered password in the function is set as newpassword
         newpass = pass1;
     }
-    const user = {
+    const user = { //creates user object
         name: name,
         newpass: newpass
     }
